5c77c4d6715e17d2f71e19518798c2256fb814cd
2019marechals <simon.marechal@student-cs.fr>
2020-06-09 16:07:33 +0200

    a less ugly movie table

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./MovieList.css"
 
 const MovieList = () => {
 	const [error, setError] = useState(null);
@@ -33,23 +34,23 @@ const MovieList = () => {
 			return <div>Loading...</div>;
 		} else {
 			return (
-        <table id="moviesTable">
-        <thead>
-          <tr>
-          <th>ID</th>
-          <th>Type</th>
-          <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-				{items.map((item) => (
-					<tr key={item.uuid}>
-						<td>{item.uuid}</td>
-						<td>{item.type}</td>
-						<td>{item.title}</td>
-					</tr>     
-          ))}
-        </tbody>
+				<table id="moviesTable">
+					<thead>
+						<tr>
+							<th id="headUuuid">ID</th>
+							<th id="headType">Type</th>
+							<th id="headTitle">Title</th>
+						</tr>
+					</thead>
+					<tbody>
+						{items.map((item) => (
+							<tr key={item.uuid}>
+								<td id="bodyUuid">{item.uuid}</td>
+								<td id="bodyType">{item.type}</td>
+								<td id="bodyTitle">{item.title}</td>
+							</tr>
+						))}
+					</tbody>
 				</table>
 			);
 		}
