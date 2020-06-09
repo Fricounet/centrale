45e9c14c4abb77161735ed49ac1971ce64a4c49d
Baptiste Girard-Carrabin <baptiste.gc@student-cs.fr>
2020-06-09 23:29:35 +0200

    setup db populating

--- backend/functions/Movies/createMovie.js
+++ backend/functions/Movies/createMovie.js
@@ -15,7 +15,7 @@ module.exports.handle = async event => {
     const dynamoDb = new DynamoDB.DocumentClient();
     const movie = {
         type: 'movies',
-        uuid:  `${count + 1}`,
+        uuid:  `test_movie_${count + 1}`,
         title: `test movie ${count + 1}`,
     }
 
