ff412b4e42f3bb42eff81010aedd9bdb823ea95d
MaelleRenaud <maelle.renaud@student-cs.fr>
2020-06-10 12:20:26 +0200

    Ugly Multiselect on saveMovie

--- client/src/components/components/SaveMovie.js
+++ client/src/components/components/SaveMovie.js
@@ -1,84 +1,65 @@
 import React, {useState} from "react";
 import { useHistory } from "react-router-dom";
+import { Multiselect } from 'react-widgets';
 
 const SaveMovie = () => {
   const [error, setError] = useState(null);
   const [isSaved, setIsSaved] = useState(false);
   const history = useHistory();
   const movieTypes = ['unknown', 'action', 'adventure', 'animation', 'children', 'comedy', 'crime', 'documentary', 'drama', 'fantasy', 'black-movie', 'horror', 'musical', 'mystery', 'romance', 'sci-fi', 'thriller', 'war', 'western'];
+  const [selectedTypes, setSelectedTypes] = useState([]);
 
   const inputTitle = React.createRef();
   const inputReleaseDate = React.createRef();
-  const inputMovieTypes = {};
-  for (var i = 0; i < movieTypes.length; i++) {
-    inputMovieTypes[movieTypes[i]] = React.createRef();
+
+  const createOpts = () => {
+    const opts = {'title': inputTitle.current.value, 'realese date': inputReleaseDate.current.value};
+    for (var i = 0; i < movieTypes.length; i++) {
+      const type = movieTypes[i];
+      if (selectedTypes.value.includes(type)) {
+        opts[type] = 1;
+      } else {
+        opts[type] = 0;
+      };
+    };
+    return opts;
   };
-  const inputAction = React.createRef();
 
   const handleSubmit = async (event) => {
     try {
-      /*const opts = {'title': inputTitle.current.value, 'realese date': inputReleaseDate.current.value};
-      for (var i = 0; i < movieTypes.length; i++) {
-        const type = movieTypes[i];
-        opts[type] = inputMovieTypes[type];
-      };*/
-      console.log(inputAction);
-      /*const response = await fetch("https://qwb4mgojyk.execute-api.eu-west-1.amazonaws.com/dev/movies/", {
+      const opts = createOpts();
+      console.log(opts);
+      /*const response = await fetch("https://y2nm5r8mg9.execute-api.eu-west-1.amazonaws.com/dev/movies/", {
         method: 'post',
         body: JSON.stringify(opts)
       });*/
       setIsSaved(true);
-      console.log(isSaved);
       setError(false);
       event.preventDefault();
-      return history.push('/save-movie');
+      return history.push('/');
     } catch (error) {
         setIsSaved(true);
         setError(error);
-        return history.push('/save-movie');
-    }
-  };
-
-  const displaySelectTypes = () => {
-    return (
-      <p>{/*
-        {movieTypes.map((type) => (
-          <>
-            <input type="checkbox" id={type} ref={inputMovieTypes[type]} />
-            <label for={type}>{type}</label><br />
-          </>
-        ))}*/}
-        <input type="checkbox" id='action' ref={inputAction} />
-        <label for='action'>action</label><br />
-      </p>
-    );
-  };
-
-  const displayForm = () => {
-    if (error) {
-			return <div>Error: {error.message}</div>;
-		} else if (isSaved == true) {
-      return <p>Le film est bien enregistré !</p>;
-		} else {
-			return (
-        <form onSubmit={handleSubmit}>
-          <label>Titre :</label><br></br>
-          <input type="text" ref={inputTitle} /><br></br>
-          <label>Date de sortie (au format 01-Jan-1995) :</label><br></br>
-          <input type="text" ref={inputReleaseDate} /><br></br>
-          <label>Type de film :</label><br></br>
-          {displaySelectTypes()}
-          <br></br>
-          <input type="submit" value="Enregistrer" />
-        </form>
-      );
+        return history.push('/');
     }
   };
 
   return (
     <div>
       <h2>Enregistrer un nouveau film</h2>
-      {displayForm()}
+      <form onSubmit={handleSubmit}>
+        <label>Titre :</label><br />
+        <input type="text" ref={inputTitle} /><br />
+        <label>Date de sortie (au format 01-Jan-1995) :</label><br />
+        <input type="text" ref={inputReleaseDate} /><br />
+        <label>Type de film :</label><br />
+        <Multiselect 
+          data={movieTypes} 
+          onChange={value => setSelectedTypes({value})}
+        />
+        <br />
+        <input type="submit" value="Enregistrer" />
+      </form>
     </div>
   );
 
