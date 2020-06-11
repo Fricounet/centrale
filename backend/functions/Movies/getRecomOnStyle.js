9534cd50bdbc44d3dc1dc7388e01767468a549c9
Minosity_VR <simon.marechal@student-cs.fr>
2020-06-11 11:14:00 +0200

    continue recomm on style

--- backend/functions/Movies/getRecomOnStyle.js
+++ backend/functions/Movies/getRecomOnStyle.js
@@ -3,17 +3,35 @@ const getRatings = require('../Ratings/getRatings');
 const serchMovieByCriteria = require('../Search/searchMoviesByCriteria');
 
 module.exports.handle = async event => {
-    console.log(event);
-    const user = event.multiValueQueryStringParameters.user;
-    console.log(user);
-    const userRatings = user != undefined ? await getRatings.handle(user) : [];
-    console.log(userRatings);
+    const userId = event.multiValueQueryStringParameters.user;
+    const userRatings = userId != undefined ? JSON.parse((await getRatings.handle({"multiValueQueryStringParameters": {"user" : userId}})).body).Ratings : [];
+    const preferedRatedMovies = userRatings.filter( movieRating => parseInt(movieRating.rating) > 3 );
+    const moviesUuid = [];
+    preferedRatedMovies.forEach(movie => moviesUuid.push(movie.uuid.slice(0, - (userId[0].length + 1))));
+
+    const dynamoDb = new DynamoDB.DocumentClient();
+    const moviesInfo = [];
+    moviesUuid.forEach(async movieUuid => {
+        console.log(movieUuid);
+        const info = await dynamoDb.get({
+            TableName: process.env.tableName,
+            Key: {
+                type: 'movie',
+                uuid: movieUuid,
+            },
+        }).promise();
+        console.log(info);
+        moviesInfo.push(info);
+    });
+
+    console.log(moviesInfo);
+
 
     return {
         statusCode: 200,
         headers: {
             'Access-Control-Allow-Origin': '*',
         },
-        body: JSON.stringify(userRatings)
+        body: JSON.stringify(moviesUuid)
     }
 }
